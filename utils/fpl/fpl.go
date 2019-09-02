67cbd7c9acd1e3cc6d9bc7240fe1cb6c519a0115
Jonathan King <jonno@jonnoking.com>
2019-09-02 23:29:23 +0100

    FPL clean

--- utils/fpl/fpl.go
+++ utils/fpl/fpl.go
@@ -13,18 +13,7 @@ import (
 	"time"
 )
 
-/*
-	Get Client
-	Refresh Cookies
-	Get Cookies from Cache
-	Check Cache Expiry
-	Save Cookies to Cache
-
-*/
-
 func main() {
-
-	//loginFPL()
 
 	// cookies, err := RefreshCookies()
 	// if err != nil {
@@ -43,6 +32,7 @@ func main() {
 
 }
 
+//FPLCookie for persisting auth cookies
 type FPLCookie struct {
 	Name       string    `json:"name"`
 	Value      string    `json:"value"`
@@ -55,6 +45,7 @@ type FPLCookie struct {
 	Acquired   time.Time `json:"acquired`
 }
 
+//GetMyTeam retrive my team from FPL
 func GetMyTeam() {
 	apiURL := "https://fantasy.premierleague.com/api/my-team/1759299/"
 
@@ -201,7 +192,7 @@ func CacheCookies(cookies map[string]FPLCookie) error {
 	return nil
 }
 
-// ReadCookies reads the cookies from file and into memory
+// ReadCookieCache reads the cookies from file and into memory
 func ReadCookieCache() (map[string]FPLCookie, error) {
 	c := map[string]FPLCookie{}
 	cookies, err := ioutil.ReadFile("./fpl-auth-cache.json")
@@ -233,132 +224,6 @@ func ValidateCookies(cookies map[string]FPLCookie) (bool, error) {
 	}
 
 	return true, nil
-}
-
-func loginFPL() {
-
-	loginURL := "https://users.premierleague.com/accounts/login/"
-
-	data := url.Values{}
-	data.Set("password", "")
-	data.Set("login", "jonno.king@gmail.com")
-	data.Set("redirect_uri", "https://fantasy.premierleague.com/")
-	data.Set("app", "plfpl-web")
-
-	u, _ := url.ParseRequestURI(loginURL)
-
-	log.Printf(u.String())
-	log.Println("URL: ", data.Encode())
-
-	client := &http.Client{
-		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			// required to ensure that the cookies are accessible
-			return http.ErrUseLastResponse
-		}}
-
-	r, _ := http.NewRequest(http.MethodPost, u.String(), strings.NewReader(data.Encode()))
-	r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	r.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
-	// required otherwise proxy will intercept request
-	r.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:40.0) Gecko/20100101 Firefox/40.0'")
-
-	resp, respErr := client.Do(r)
-	check(respErr)
-	defer resp.Body.Close()
-
-	byteValue, readErr := ioutil.ReadAll(resp.Body)
-	if readErr != nil {
-		log.Fatal(readErr)
-	}
-
-	fErr := ioutil.WriteFile("./loginpage.html", byteValue, 0644)
-	check(fErr)
-
-	log.Printf(resp.Status)
-	//log.Println(resp.Cookies)
-	log.Println(resp.Header)
-	log.Println(resp.ContentLength)
-
-	log.Println(resp.Header.Get("Server"))
-	log.Println(resp.Header.Get("Content-Type"))
-	log.Println(resp.Header.Get("Last-Modified"))
-	log.Println(resp.Header.Get("ETag"))
-	log.Println(resp.Header.Get("Content-Encoding"))
-
-	log.Println("Number of cookies: ", len(resp.Cookies()))
-
-	d := resp.Cookies()
-
-	// c := []FPLCookie{}
-
-	// log.Println("--- PRINTING COOKIES ---")
-	// for _, cookie := range resp.Cookies() {
-	// 	log.Printf(cookie.Name)
-	// 	log.Println(cookie.Value)
-	// 	log.Println(cookie.Domain)
-	// 	cc := FPLCookie{
-	// 		Name:       cookie.Name,
-	// 		Value:      cookie.Value,
-	// 		Domain:     cookie.Domain,
-	// 		HttpOnly:   cookie.HttpOnly,
-	// 		MaxAge:     cookie.MaxAge,
-	// 		Path:       cookie.Path,
-	// 		RawExpires: cookie.RawExpires,
-	// 		Secure:     cookie.Secure,
-	// 	}
-	// 	c = append(c, cc)
-	// }
-
-	getMyTeamOld(d)
-}
-
-func getMyTeamOld(cookies []*http.Cookie) {
-	apiURL := "https://fantasy.premierleague.com/api/my-team/1759299/"
-
-	log.Println("--- PRINTING COOKIES AGAIN ---")
-	for _, cookie := range cookies {
-		log.Printf(cookie.Name)
-		log.Println(cookie.Value)
-		log.Println(cookie.Domain)
-		log.Println(cookie.Path)
-		log.Println(cookie.Secure)
-		log.Println(cookie.RawExpires)
-	}
-
-	client := &http.Client{}
-	r, _ := http.NewRequest(http.MethodGet, apiURL, nil)
-	for _, cookie := range cookies {
-		if cookie.Name != "elevate" {
-			r.AddCookie(cookie)
-			// r.AddCookie(&http.Cookie{
-			// 	Name:   cookie.Name,
-			// 	Value:  cookie.Value,
-			// 	Domain: cookie.Domain,
-			// 	Path:   cookie.Path,
-			// })
-		}
-	}
-	r.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:40.0) Gecko/20100101 Firefox/40.0'")
-
-	resp, respErr := client.Do(r)
-	check(respErr)
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		log.Panic("\nStatus Code: ", resp.StatusCode)
-		log.Println(resp.Status)
-		return
-	}
-
-	byteValue, readErr := ioutil.ReadAll(resp.Body)
-	if readErr != nil {
-		log.Fatal(readErr)
-	}
-
-	fErr := ioutil.WriteFile("./myteam.json", byteValue, 0644)
-	check(fErr)
-
 }
 
 func callGetBootstrap() map[string]interface{} {
